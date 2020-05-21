539efc3a4b7d0a2f7d11b546358c1fe963424d80
Ron van Wier <ron@van-wier.org>
2020-05-21 17:18:47 +0200

    blink white

--- mqttminimal.py
+++ mqttminimal.py
@@ -6,26 +6,36 @@ import unit
 import json
 import wifiCfg
 
+def blink(color):
+    if color=='red':
+        setScreenColor(0xFF0000)
+    if color=='green':
+        setScreenColor(0x00FF00)
+    if color=='yellow':
+        setScreenColor(0xFFFF00)
+    if color=='white':
+        setScreenColor(0xFFFFFF)
+    wait(1)
+    setScreenColor(0x000000)    
+        
 def wificheck():
-    while !wifiCfg.wlan_sta.isconnected():
-        wifiCfg.screenShow()
-        wifiCfg.autoConnect(lcdShow = True)
-        #wifiCfg.reconnect()
-
-#setScreenColor(0xFF0000)
-
+    while not wifiCfg.wlan_sta.isconnected():
+        blink('red')
+        #wifiCfg.screenShow()
+        #wifiCfg.autoConnect(lcdShow = False)
+        wifiCfg.reconnect()
+    blink('white')    
+    
 wificheck()
 
 m5mqtt = M5mqtt('mqttenv', '192.168.0.129', 1883, '', '', 300)
 m5mqtt.start()
 
 while True:
-    setScreenColor(0x000000)
     wificheck()
-
     t_dict = { "ticks": time.ticks_ms(), 'msg': 'hope' }
     s_dict = json.dumps(t_dict)
     m5mqtt.publish('timer/ticks',s_dict)
-    setScreenColor(0x00FF00)
+    blink('green')
     
-    wait(10)
+    wait(5)
